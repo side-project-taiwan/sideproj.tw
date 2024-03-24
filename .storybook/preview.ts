5b0f03ca2894a089eb846e374cf7e1f531337f15
hcw0915 <love233031@gmail.com>
2024-03-25 00:05:07 +0800

    fix: change tailwind exports from ESM to CJS & add config in storybook preview.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,16 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react'
+import '../app/globals.css'
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
-};
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/
+			}
+		}
+	}
+}
 
-export default preview;
+export default preview
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}'
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./stories/**/*.{js,ts,jsx,tsx,mdx}'
 	],
 	theme: {
 		extend: {
@@ -17,4 +17,3 @@ const config: Config = {
 	},
 	plugins: []
 }
-export default config
