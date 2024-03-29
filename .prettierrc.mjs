522fe7151dc803e51b864e0577bad5470c4c7609
arthur <arthur.oceanus@gmail.com>
2024-03-29 09:55:47 +0800

    Add prettier config

---
+++ .prettierrc.mjs
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  printWidth: 80,
+}
+
+export default config
