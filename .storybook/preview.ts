bdaf64024b05ba2920f6d27391154259fd445ea7
Zero <82575487+arthur-mountain@users.noreply.github.com>
2024-04-13 16:04:35 +0800

    Feat/Add pull request template (#26)
    * Add pull request template
    
    * Review changed
    
    Append new check list item
    
    * Update check list
    
    * Init format with prettier fix
    
    * Remove test command before test setup

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,15 +2,15 @@ import type { Preview } from '@storybook/react'
 import '../app/globals.css'
 
 const preview: Preview = {
-	parameters: {
-		actions: { argTypesRegex: '^on[A-Z].*' },
-		controls: {
-			matchers: {
-				color: /(background|color)$/i,
-				date: /Date$/
-			}
-		}
-	}
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
 }
 
 export default preview
